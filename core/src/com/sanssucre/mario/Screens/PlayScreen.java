1c3ddaf4a3f49b2dc3b9dfaeb23d875740855367
TANG Wenjia <wenjia.tang@telecomnancy.eu>
2023-10-29 23:33:44 +0100

    :sparkles: add new play screen

---
+++ core/src/com/sanssucre/mario/Screens/PlayScreen.java
@@ -0,0 +1,56 @@
+package com.sanssucre.mario.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.sanssucre.mario.SuperMario;
+
+public class PlayScreen implements Screen {
+    private SuperMario game;
+    private Texture texture;
+    public PlayScreen(SuperMario game){
+        this.game = game;
+        this.texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        this.game.batch.begin();
+        this.game.batch.draw(texture, 0, 0);
+        this.game.batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
