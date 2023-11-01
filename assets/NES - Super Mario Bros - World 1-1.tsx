53d06a1d98b731b64fd21c8dfa3018dc552c2c91
TANG Wenjia <wenjia.tang@telecomnancy.eu>
2023-11-01 15:30:30 +0100

    :bento: add map to assets

---
+++ assets/NES - Super Mario Bros - World 1-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="NES - Super Mario Bros - World 1-1" tilewidth="16" tileheight="16" tilecount="6330" columns="211">
+ <image source="NES - Super Mario Bros - World 1-1.png" width="3376" height="480"/>
+</tileset>
